08bc3b42e6bf7f6b66bfd29297e9a638c1252248
wusidixi <robinzxd@163.com>
2021-04-18 23:59:29 +0800

    wusidixi-gai-1

--- test.py
+++ test.py
@@ -5,3 +5,8 @@ from tkinter import *
 from tkinter import messagebox
 import time
 import json
+
+from openpyxl import Workbook, load_workbook
+import collections
+import matplotlib.pyplot as plt  #导入matplotlib.pyplot模块，别名取为plt
+import numpy as np          #导入numpy库，别名起为np
