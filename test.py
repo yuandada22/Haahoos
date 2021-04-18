4bef2d2b25c2f30b3a600031052d481723e638be
yuandada22 <19377241@buaa.edu.cn>
2021-04-19 00:01:45 +0800

    modify test2

--- test.py
+++ test.py
@@ -10,3 +10,12 @@ from openpyxl import Workbook, load_workbook
 import collections
 import matplotlib.pyplot as plt  #导入matplotlib.pyplot模块，别名取为plt
 import numpy as np          #导入numpy库，别名起为np
+
+import pylab
+from scipy import interpolate
+from matplotlib.font_manager import FontProperties
+import jieba
+from wordcloud import WordCloud
+import PIL.Image as image
+pylab.mpl.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus']=False # 用来正常显示负号
